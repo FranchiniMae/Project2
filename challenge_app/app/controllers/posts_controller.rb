d7bf16afde2f1e19f00e46d32541c5be53a3f75a
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-23 19:48:09 -0800

    Adds edit posts methods

--- challenge_app/app/controllers/posts_controller.rb
+++ challenge_app/app/controllers/posts_controller.rb
@@ -11,15 +11,40 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		p @post
 		post_params = params.require(:post).permit(:comment, :picture, :challenge_id)
-		@post = Post.create(post_params)
+		@post = Post.new(post_params)
 		challenge = @post.challenge_id
 		if @post.save
 			redirect_to challenge_path(challenge)
 		else
 			render :new
 		end 
+	end 
+
+	def show
+		@post = Post.find(params[:id])
+		render :show
+	end 
+
+	def edit
+		@post = Post.find(params[:id])
+		render :edit
+	end
+
+	def update
+		post_params = post_params = params.require(:post).permit(:comment, :picture, :challenge_id)
+		@post = Post.find(params[:id])
+		@post.update_attributes(post_params)
+		@post.save
+		challenge = @post.challenge_id
+		redirect_to challenge_path(challenge)
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		challenge = @post.challenge_id
+		redirect_to challenge_path(challenge)
 	end 
 
 end
